d7867c5fd18b69afabe78f70a082d313ecb1c3fd
khoirul arifin <khoirularfin12@gmail.com>
2023-12-15 19:43:30 +0700

    test deploy

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+/app/migrate -path /app/migration -database postgresql://root:167916@postgres:5432/roomate?sslmode=disable -verbose up
 
 echo "start the app"
 exec "$@"
